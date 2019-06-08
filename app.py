043aa5e9a672ad2ebf4a556ad139146adfa669b8
qiuyue <qiuyue9971@126.com>
2019-06-09 06:51:38 +0800

    the lastest version

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from Config import config
-from auth import Auth, auth_login
+from auth import Auth, verify_login
 from message import Message
 from subject import Sub
 from user_info import UserInfo
@@ -11,10 +11,10 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config["Develop"])
-    Message.before_request(auth_login)
-    Sub.before_request(auth_login)
-    UserInfo.before_request(auth_login)
-    SecretMessage.before_request(auth_login)
+    Message.before_request(verify_login)
+    Sub.before_request(verify_login)
+    UserInfo.before_request(verify_login)
+    SecretMessage.before_request(verify_login)
     app.register_blueprint(Auth, url_prefix="/auth")
     app.register_blueprint(Message, url_prefix="/message")
     app.register_blueprint(Sub, url_prefix="/sub")
