3d2c719a595d4b4aea7920c109b4ad8bb213f635
lishuxiang <lishuxiang@cug.edu.cn>
2019-06-08 14:54:13 +0800

    adjust subject

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from Config import config
 from auth import Auth, auth_login
+from post import Post
+from subject import Sub
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config["Develop"])
     app.register_blueprint(Auth, url_prefix="/auth")
+    app.register_blueprint(Post, url_prefix="/post")
+    app.register_blueprint(Sub, url_prefix="/sub")
     Auth.before_request(auth_login)
+    Post.before_request(auth_login)
+    Sub.before_request(auth_login)
     return app
 
 
